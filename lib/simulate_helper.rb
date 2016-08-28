d00243e9bd224058128aed1e1c3baa7a3a7de5e0
joepshoulak <joepshoulak2@gmail.com>
2016-08-28 01:15:18 -0500

    refactor

--- lib/simulate_helper.rb
+++ lib/simulate_helper.rb
@@ -5,7 +5,33 @@ def algorithm(subgame1, subgame2)
   return (subgame1.points - subgame2.points).abs
 end
 
+# Misc.
+def clear_line
+  print "\r" + " "*100 + "\b"*100
+end
+
 # Classes
+class CLI
+  def initialize
+    @args = Hash.new
+    
+    if ARGV.empty?
+      puts "Error: No arguments"
+      exit
+    end
+  end
+  
+  attr_accessor :args
+  
+  def add_arg(key, value)
+    @args[key] = value
+  end
+  
+  def change_arg(key, new_value)
+    self.args[key] = new_value
+  end
+end
+
 class Subgame
   def initialize
     @points = 0
@@ -67,78 +93,263 @@ class Match
   end
 end
 
-# Misc.
-def clear_line
-  print "\r" + " "*100 + "\b"*100
-end
+class Simulation
+  def initialize(sport, year, periods, verbose)
+    @sport = sport
+    @year = year
+    @periods = periods
+    @verbose = verbose
+    
+    @total = 0
+    @correct = 0
+    @unknown = 0
+    
+    @json_url = "./data/#{self.sport}#{self.year}.json"
 
-# Parse game
-def process(game, periods, sport)
-  subgame_home = Subgame.new
-  subgame_away = Subgame.new 
-
-  subgame_home.id = game["id"]
-  subgame_away.id = game["id"]
-  
-  case sport.upcase
-  when "NFL"
-    home = game["summary"]["home"]
-    away = game["summary"]["away"]
-  when "NBA"
-    home = game["home"]
-    away = game["away"]
-  end
-
-  home_market = home["market"]
-  away_market = away["market"]
-    
-  home_name = home["name"]
-  away_name = away["name"]
-  
-  subgame_home.name = home_market + " " + home_name
-  subgame_away.name = away_market + " " + away_name
-  
-  subgame_home.points = 0
-  subgame_away.points = 0
-    
-  game["periods"].length.times do |pe| # For each period
-    period = game["periods"][pe]
-    
-    home_points = period["scoring"]["home"]["points"].to_i
-    away_points = period["scoring"]["away"]["points"].to_i
-    
-    subgame_home.points += home_points unless pe >= periods
-    subgame_away.points += away_points unless pe >= periods
-    
-    subgame_home.final_score += home_points
-    subgame_away.final_score += away_points
+    puts "Loading File..."
+    @matches = self.json_load
+    clear_line
   end
   
-  m = Match.new(subgame_home, subgame_away)
+  attr_accessor :matches, :total, :correct, :unknown, :sport, :year, :periods, :verbose, :json_url
   
-  m.true_winner = [subgame_home, subgame_away].sort_by { |s| s.final_score }[1]
-  m.true_tie = ( subgame_home.final_score == subgame_away.final_score )
+  def process(game, periods, sport)
+    subgame_home = Subgame.new
+    subgame_away = Subgame.new 
+
+    subgame_home.id = game["id"]
+    subgame_away.id = game["id"]
+  
+    case sport.upcase
+    when "NFL"
+      home = game["summary"]["home"]
+      away = game["summary"]["away"]
+    when "NBA"
+      home = game["home"]
+      away = game["away"]
+    end
+
+    home_market = home["market"]
+    away_market = away["market"]
+    
+    home_name = home["name"]
+    away_name = away["name"]
+  
+    subgame_home.name = home_market + " " + home_name
+    subgame_away.name = away_market + " " + away_name
+  
+    subgame_home.points = 0
+    subgame_away.points = 0
+    
+    game["periods"].length.times do |pe| # For each period
+      period = game["periods"][pe]
+    
+      home_points = period["scoring"]["home"]["points"].to_i
+      away_points = period["scoring"]["away"]["points"].to_i
+    
+      subgame_home.points += home_points unless pe >= periods
+      subgame_away.points += away_points unless pe >= periods
+    
+      subgame_home.final_score += home_points
+      subgame_away.final_score += away_points
+    end
+  
+    m = Match.new(subgame_home, subgame_away)
+  
+    m.true_winner = [subgame_home, subgame_away].sort_by { |s| s.final_score }[1]
+    m.true_tie = ( subgame_home.final_score == subgame_away.final_score )
       
-  return m
-end
-
-# Parse season
-def json_load(json_file, periods, sport)
-  processed_games = []
+    return m
+  end
+    
+  def json_load
+    processed_games = []
   
-  games = JSON.parse(File.read(json_file))
-  games = games.select { |g| g["status"] == "closed" }
+    games = JSON.parse(File.read(self.json_url))
+    games = games.select { |g| g["status"] == "closed" }
   
-  games.each do |game|
-    processed_games << process(game, periods, sport)
+    games.each do |game|
+      processed_games << process(game, self.periods, self.sport)
+    end
+  
+    return processed_games
+  end  
+      
+  def load_matches!
+    puts "Loading File..."
+    clear_line
   end
   
-  return processed_games
+  def simulate
+    print "Loading File..."
+
+    self.matches.select { |m| !m.true_tie }.each do |match|
+      self.total += 1
+
+      if !match.tie? && !match.true_tie
+        self.correct += 1 if match.winner.name == match.true_winner.name
+  
+        if self.verbose
+          match.subgames.each do |subgame|
+            puts "#{subgame.name}"
+            puts "\tCheck Score: #{subgame.points}"
+            puts "\tFinal Score: #{subgame.final_score}"
+          end
+    
+          puts "Prediction: "
+          puts "\t#{match.winner.name} are ahead by #{match.spread.abs}"
+    
+          match.subgames.each do |subgame|
+            puts "\t#{subgame.name}: #{subgame.win_percentage}%"
+          end
+    
+          puts "\tResult: #{match.winner.name == match.true_winner.name}"
+          puts 
+        end
+      else
+        self.unknown += 1
+      end
+    end
+
+    clear_line  
+
+    correct_ratio = correct.to_f/total
+    unknown_ratio = unknown.to_f/total
+    theoretical_ratio = correct.to_f/(total - unknown)
+
+    correct_percent = 100*correct_ratio.round(2)
+    unknown_percent = 100*unknown_ratio.round(2)
+    theoretical_percent = 100*theoretical_ratio.round(2)
+
+    message = "Results: #{periods} period(s), #{correct_percent}% accurate, #{unknown_percent}% unknown, #{theoretical_percent}% theoretical."
+    numbers = [correct_percent, unknown_percent, theoretical_percent]
+
+    return [message] + numbers
+  end
+  
+  def run
+    self.load_matches!
+    
+    if self.periods == "all"
+      self.correct = 0
+      self.unknown = 0
+      self.theoretical = 0
+      (1..3).each do |n|
+        r = self.simulate(sport, year, n, verbose)
+        puts r[0]
+        self.correct += r[1]
+        self.unknown += r[2]
+        self.theoretical += r[3]
+      end
+      puts "Average Correct: #{(correct/3).round(2)}%"
+      puts "Average Unknown: #{(unknown/3).round(2)}%"
+      puts "Average Theoretical: #{(theoretical/3).round(2)}%"
+    else
+      puts self.simulate[0]
+    end
+  end
 end
 
-# Load files
-def convert_to_subgames(matches)  
-  subgames = matches.map { |m| m.subgames }.flatten
+class Radar
+  def initialize(sport, year)
+    @sport = sport
+    @year = year
+    @games = Array.new
+    @ids = Array.new
     
-  return subgames
+    case @sport
+    when "NBA"
+      @url = 'nba-t3'
+      @key = 'uqpj2aeucyf5erk28py2xzqu'
+    when "NFL"
+      @url = 'nfl-ot1'
+      @key = '5etueuh9u3a8auueywb7pesw'
+    end
+  end
+  
+  attr_accessor :schedule, :games, :ids, :url, :key, :sport, :year
+  
+  def get_schedule
+    print "Loading Schedule..."
+    
+    begin
+      self.schedule = JSON.parse(`curl -s http://api.sportradar.us/#{self.url}/games/#{self.year}/REG/schedule.json?api_key=#{self.key}`)
+    rescue Exception => msg
+      if msg.message.include? 'Developer Over Rate'
+        puts "Error: Too many queries this month"
+        exit
+      elsif msg.message.include? 'Developer Over Qps'
+        sleep 1
+        self.schedule = JSON.parse(`curl -s http://api.sportradar.us/#{self.url}/games/#{self.year}/REG/schedule.json?api_key=#{self.key}`)
+      else
+        puts msg.message
+        exit
+      end
+    end
+    
+    clear_line
+  end
+  
+  def get_ids
+    case sport
+    when "NFL"  
+      schedule["weeks"].each do |w|
+        w["games"].each do |g|
+          self.ids +=  [g["id"]]
+        end
+      end
+    when "NBA"
+      schedule["games"].each do |g|
+        self.ids +=  [g["id"]]
+      end  
+    end  
+  end
+  
+  def get_games
+    print "Loading games..."
+
+    n = 1
+    l = 5
+    #l = game_ids.length
+    
+    self.ids.each do |id|
+      clear_line
+    
+      print "Loading game #{n}/#{l} (#{(100.0*n/l).round}%)..."
+    
+      exit if n == l
+      n += 1
+    
+      begin
+        game = JSON.parse(`curl -s http://api.sportradar.us/#{self.url}/games/#{id}/pbp.json?api_key=#{self.key}`)
+      rescue Exception => msg
+        if msg.message.include? 'Developer Over Rate'
+          puts "Error: Too many queries this month"
+          exit
+        elsif msg.message.include? 'Developer Over Qps'
+          sleep 1
+          game = JSON.parse(`curl -s http://api.sportradar.us/#{self.url}/games/#{id}/pbp.json?api_key=#{self.key}`)
+        else
+          puts msg.message
+          game = nil
+        end
+      end
+      print "\b"*30
+      self.games += [game]
+    end
+  
+    self.games = games.select { |g| !g.nil? }
+    
+    clear_line
+  end
+  
+  def save_games
+    print "Saving games..."
+    
+    f = File.open("./data/#{self.sport}#{self.year}.json", "w")
+    f.write(self.games.to_json)
+    
+    clear_line
+    puts "Games saved."
+  end
 end
--- simulate.rb
+++ simulate.rb
@@ -1,211 +1,25 @@
 require './lib/simulate_helper'
 
-def simulate(sport, year, periods, verbose)
-  total   = 0
-  correct = 0
-  unknown = 0
-
-  print "Loading Testing File..."
-
-  matches = json_load("./data/#{sport}2015.json", periods, sport)
-
-  clear_line
-
-  matches.select { |m| !m.true_tie }.each do |match|
-    total += 1
-  
-    if !match.tie? && !match.true_tie
-      correct += 1 if match.winner.name == match.true_winner.name
-    
-      if verbose
-        match.subgames.each do |subgame|
-          puts "#{subgame.name}"
-          puts "\tCheck Score: #{subgame.points}"
-          puts "\tFinal Score: #{subgame.final_score}"
-        end
-      
-        puts "Prediction: "
-        puts "\t#{match.winner.name} are ahead by #{match.spread.abs}"
-      
-        match.subgames.each do |subgame|
-          puts "\t#{subgame.name}: #{subgame.win_percentage}%"
-        end
-      
-        puts "\tResult: #{match.winner.name == match.true_winner.name}"
-        puts 
-      end
-    else
-      unknown += 1
-    end
-  end
-
-  clear_line  
-
-  correct_ratio = correct.to_f/total
-  unknown_ratio = unknown.to_f/total
-  theoretical_ratio = correct.to_f/(total - unknown)
-
-  correct_percent = 100*correct_ratio.round(2)
-  unknown_percent = 100*unknown_ratio.round(2)
-  theoretical_percent = 100*theoretical_ratio.round(2)
-
-  message = "Results: #{periods} period(s), #{correct_percent}% accurate, #{unknown_percent}% unknown, #{theoretical_percent}% theoretical."
-  numbers = [correct_percent, unknown_percent, theoretical_percent]
-
-  return [message] + numbers
-end
-
-def menu
-  sport = nil
-  year = nil
-  periods = nil
-  verbose = nil
-  
-  while sport.nil?
-    puts "Sports:"
-    puts "\t1: NFL"
-    puts "\t2: NBA"
-
-    print "\nChoice: "
-    
-    sport = STDIN.gets.chomp.to_i
-    
-    case sport
-    when 1
-      sport = "NFL"
-    when 2
-      sport = "NBA"
-    end
-  end
-  
-  puts
-  
-  while year.nil?
-    it = 1
-    
-    Dir["data/*"].select { |f| f.include? sport}.reverse.each do |file|
-      puts file.gsub("data/", "\t#{it}: ")
-      it += 1
-    end
-    
-    print "\nChoice: "
-    
-    year = STDIN.gets.chomp
-    
-    if year == "all"
-      year = "all"
-    elsif year.to_i.to_s == year
-      year = year.to_i
-    end
-  end
-  
-  puts
-  
-  while periods.nil?
-    puts "Periods:"
-    puts "\t1: 1 Period"
-    puts "\t2: 2 Periods"
-    puts "\t3: 3 Periods"
-    puts "\t4: All of the above"
-
-    print "\nChoice: "
-    
-    periods = STDIN.gets.chomp
-    
-    periods = periods.to_i if periods.to_i.to_s == periods
-    
-    periods = "all" if periods == 4
-  end
-  
-  while verbose.nil?
-    puts "verbose:"
-    puts "\t1: true"
-    puts "\t2: false"
-
-    print "\nChoice: "
-    
-    verbose = STDIN.gets.chomp.to_i
-    
-    case verbose
-    when 1
-      verbose = true
-    when 2
-      verbose = false
-    end
-  end
-  
-  if periods == "all"
-    correct = 0
-    unknown = 0
-    theoretical = 0
-    (1..3).each do |n|
-      r = simulate(sport, year, n, verbose)
-      puts r[0]
-      correct += r[1]
-      unknown += r[2]
-      theoretical += r[3]
-    end
-    puts "Average Correct: #{(correct/3).round(2)}%"
-    puts "Average Unknown: #{(unknown/3).round(2)}%"
-    puts "Average Theoretical: #{(theoretical/3).round(2)}%"
-  else
-    puts simulate(sport, year, periods, verbose)[0]
-  end
-end
-
 if ARGV.empty?
   puts "Needs arguments"
   puts "simulate.rb SPORT YEAR PERIODS(/ALL) (VERBOSE)"
   exit
 end
 
-if ARGV[0] == "menu"
-  menu
-elsif "NFL, NBA".include? ARGV[0].upcase
-  sport = ARGV[0].upcase
-  
-  if ARGV[1].to_i.to_s == ARGV[1]
-    year = ARGV[1].to_i
-  else
-    puts "Needs year"
-    puts "simulate.rb SPORT YEAR PERIODS(/ALL) (VERBOSE)"
-    exit
-  end
-  
-  if ARGV[2].to_i.to_s == ARGV[2]
-    periods = ARGV[2].to_i
-  elsif ARGV[2] == "all"
-    periods = "all"
-  else
-    puts "Needs periods"
-    puts "simulate.rb SPORT YEAR PERIODS(/ALL) (VERBOSE)"
-    exit
-  end
-  
-  if ARGV[3] == "verbose"
-    verbose = true
-  else
-    verbose = false
-  end
-     
-  if periods == "all"
-    correct = 0
-    unknown = 0
-    theoretical = 0
-    (1..3).each do |n|
-      r = simulate(sport, year, n, verbose)
-      puts r[0]
-      correct += r[1]
-      unknown += r[2]
-      theoretical += r[3]
-    end
-    puts "Average Correct: #{(correct/3).round(2)}%"
-    puts "Average Unknown: #{(unknown/3).round(2)}%"
-    puts "Average Theoretical: #{(theoretical/3).round(2)}%"
-  else
-    puts simulate(sport, year, periods, verbose)[0]
-  end
-else 
-  puts "Sport unsupported"
-  puts "simulate.rb SPORT YEAR PERIODS(/ALL) (VERBOSE)"
-end
+values = CLI.new
+
+values.add_arg("sport", ARGV[0])
+values.add_arg("year",  ARGV[1].to_i)
+values.add_arg("periods", ARGV[2])
+values.change_arg("periods", values.args["periods"].to_i) if values.args["periods"].to_i.to_s == values.args["periods"]
+values.add_arg("verbose", ARGV[3])
+values.change_arg("verbose", values.args["verbose"] == "true")
+
+sport   = values.args["sport"]
+year    = values.args["year"]
+periods = values.args["periods"]
+verbose = values.args["verbose"]
+
+s = Simulation.new(sport, year, periods, verbose)
+
+s.run
